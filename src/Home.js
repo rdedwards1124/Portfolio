cc413d0b3aa77ca802b6ae9fcf74d4eb788a8598
Rashonda Edwards <rashondaedwards@aol.com>
2023-11-21 12:26:21 -0800

    more points

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,21 @@ function Home (){
         <>
             <div>
                 <div>my prof pic</div>
-                <div>my basic info</div>
-                <div>personal statement</div>
+                <div>my basic info
+                    <ul>
+                        <li>Name: Rashonda Edwards</li>
+                        <li>Occupation: Software Developer</li>
+                        <li>Skills:
+                            <ul>
+                                <li>Programming Languages - Javascript, Python, SQL</li>
+                                <li>Tools - React, Node.js, Express, PSQL, Git, GitHub</li>
+                                <li>Databases - Postgres, MongoDB</li>
+                                <li>Knowledge - HTTP operations, RESTful APIs, Cybersecurity</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div>My Personal Statement</div>
             </div>
         </>
     )
