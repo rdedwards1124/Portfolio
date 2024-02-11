a40aa584c3f5c67eb0da1df51b4f33a03d10c5e1
Rashonda Edwards <rashondaedwards@aol.com>
2024-02-11 15:25:34 -0800

    more project work

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,9 +6,19 @@ function Projects() {
         <>
             <div className="Home-Main">
                 <div className="Paragraph">
-                    <h3>My Personal Statement:</h3>
+                    <h3>Pokemon Pipeline:</h3>
                     <p>
-                        
+                        A user-friendly Pokémon themed Web Application for gamers of any level or any Pokémon fan to gather, trade, and share Pokémon knowledge. 
+                    </p>
+                    <br/>
+                    <h3>Pokemon Pipeline:</h3>
+                    <p>
+                        A user-friendly Pokémon themed Web Application for gamers of any level or any Pokémon fan to gather, trade, and share Pokémon knowledge. 
+                    </p>
+                    <br/>
+                    <h3>Pokemon Pipeline:</h3>
+                    <p>
+                        A user-friendly Pokémon themed Web Application for gamers of any level or any Pokémon fan to gather, trade, and share Pokémon knowledge. 
                     </p>
                 </div>
             </div>
