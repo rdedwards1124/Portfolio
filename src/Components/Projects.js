4ee9b8b99f03e9053444536b9f89ea773782c4a2
Rashonda Edwards <rashondaedwards@aol.com>
2024-02-13 16:08:11 -0800

    projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,12 +11,12 @@ function Projects() {
                         A user-friendly Pokémon themed Web Application for gamers of any level or any Pokémon fan to gather, trade, and share Pokémon knowledge. 
                     </p>
                     <br/>
-                    <h3>Pokemon Pipeline:</h3>
+                    <h3>Movie Search Website:</h3>
                     <p>
-                        A user-friendly Pokémon themed Web Application for gamers of any level or any Pokémon fan to gather, trade, and share Pokémon knowledge. 
+                        A Web Application that is a fun search feature for movies, tv shows, and video games. Gather information and store your favorites.
                     </p>
                     <br/>
-                    <h3>Pokemon Pipeline:</h3>
+                    <h3>Maze Game:</h3>
                     <p>
                         A user-friendly Pokémon themed Web Application for gamers of any level or any Pokémon fan to gather, trade, and share Pokémon knowledge. 
                     </p>
